407df652a636a7bc89d54ee1725f0d71b9cdc63a
Hakka <adwfh25@gmail.com>
2026-05-27 01:37:59 +0000

    Added anime search

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,16 +3,20 @@ import AnimeCard from '../components/AnimeCard';
 import { fetchTopAnime, searchAnime } from '../api/animeApi';
 
 export default function Home() {
-  const [anime, setAnime] = useState<any[]>([]);
-  const [query, setQuery] = useState('');
+  let anime: any[] = []
+  const setAnime = (val: any[]) => {   //Функция компонента вызывается заново, каждый раз новый (anime) 
+    anime = val                        //Не подходит нам
+  }
+  const [anime, setAnime] = useState<any[]>([]); // useState добавляет и отслеживает состояние
+  const [query, setQuery] = useState(''); // Почему именно useState - запоминает состояние между рендерами и автоматически перерисовывает компонент
 
-  useEffect(() => {
-    fetchTopAnime().then(setAnime);
+  useEffect(() => {                 //useEffect выполняет побочные эфекты
+    fetchTopAnime().then(setAnime); //Нам нужен что бы делать запрос в setAnime после чего обновлять данные
   }, []);
 
-  const handleSearch = async () => {
-    if (!query) return;
-    const data = await searchAnime(query);
+  const handleSearch = async () => {       //можно было через then но async удобнее
+    if (!query) return;                    //Проверяем есть ли текс, делаем запрос и сохраняем в state
+    const data = await searchAnime(query); //searchAnime делаем запрос к нашему API
     setAnime(data);
   };
 
